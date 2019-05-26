ea7b7d9a7bbfe3a01e50a458bec7b9dd445de974
Vlad264 <vlad-498@yandex.ru>
2019-05-27 00:19:04 +0700

    Cretae UseCases for searching

---
+++ app/src/main/java/ru/nsu/bashev/modules/useCases/LoadAccountsByLogin.java
@@ -0,0 +1,55 @@
+package ru.nsu.bashev.modules.useCases;
+
+import java.util.List;
+
+import ru.nsu.bashev.common.useCaseEngine.UseCase;
+import ru.nsu.bashev.model.Account;
+import ru.nsu.bashev.model.Email;
+import ru.nsu.bashev.model.Login;
+import ru.nsu.bashev.modules.database.account.IAccountDBHandler;
+
+public class LoadAccountsByLogin extends UseCase<LoadAccountsByLogin.RequestValues, LoadAccountsByLogin.ResponseValues> {
+
+    private final IAccountDBHandler accountDBHandler;
+
+    public LoadAccountsByLogin(IAccountDBHandler accountDBHandler) {
+        this.accountDBHandler = accountDBHandler;
+    }
+
+    @Override
+    protected void executeUseCase(RequestValues requestValues) {
+        List<Account> accounts = accountDBHandler.getAccountsByLogin(new Login(requestValues.getLogin()));
+        if (accounts != null) {
+            ResponseValues responseValues = new ResponseValues(accounts);
+            getUseCaseCallback().onSuccess(responseValues);
+        } else {
+            getUseCaseCallback().onError();
+        }
+    }
+
+    public static final class RequestValues implements UseCase.RequestValues {
+
+        private String login;
+
+        public RequestValues(String email) {
+            this.login = login;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+    }
+
+    public static final class ResponseValues implements UseCase.ResponseValues {
+
+        private List<Account> accounts;
+
+        public ResponseValues(List<Account> accounts) {
+            this.accounts = accounts;
+        }
+
+        public List<Account> getAccounts() {
+            return accounts;
+        }
+    }
+}
---
+++ app/src/main/java/ru/nsu/bashev/modules/useCases/LoadAccountsByTitle.java
@@ -0,0 +1,54 @@
+package ru.nsu.bashev.modules.useCases;
+
+import java.util.List;
+
+import ru.nsu.bashev.common.useCaseEngine.UseCase;
+import ru.nsu.bashev.model.Account;
+import ru.nsu.bashev.model.Email;
+import ru.nsu.bashev.modules.database.account.IAccountDBHandler;
+
+public class LoadAccountsByTitle extends UseCase<LoadAccountsByTitle.RequestValues, LoadAccountsByTitle.ResponseValues> {
+
+    private final IAccountDBHandler accountDBHandler;
+
+    public LoadAccountsByTitle(IAccountDBHandler accountDBHandler) {
+        this.accountDBHandler = accountDBHandler;
+    }
+
+    @Override
+    protected void executeUseCase(RequestValues requestValues) {
+        List<Account> accounts = accountDBHandler.getAccountsByTitle(requestValues.getTitle());
+        if (accounts != null) {
+            ResponseValues responseValues = new ResponseValues(accounts);
+            getUseCaseCallback().onSuccess(responseValues);
+        } else {
+            getUseCaseCallback().onError();
+        }
+    }
+
+    public static final class RequestValues implements UseCase.RequestValues {
+
+        private String title;
+
+        public RequestValues(String email) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+
+    public static final class ResponseValues implements UseCase.ResponseValues {
+
+        private List<Account> accounts;
+
+        public ResponseValues(List<Account> accounts) {
+            this.accounts = accounts;
+        }
+
+        public List<Account> getAccounts() {
+            return accounts;
+        }
+    }
+}
